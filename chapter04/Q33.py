f7eff232d742a524b80aeeba0274cb8ff14e4a4b
trtd56 <5toda6@gmail.com>
2019-02-03 22:27:35 +0900

    fix and add W33

--- chapter04/Q33.py
+++ chapter04/Q33.py
@@ -8,17 +8,8 @@ with open('neko.txt', 'r') as f:
         text_elem = tagger.parse(line)
         words_info = text_elem.split('\n')
         words_info = [w for w in words_info if w not in ['EOS', '']]
-        plt_words = []
         for w_info in words_info:
             word, _, _, pos, _, _ = w_info.split('\t')
-            pos = pos.split('-')[0]
-            if pos == '名詞' and len(plt_words) == 0:
-                plt_words.append(word)
-                continue
-            elif word == 'の' and len(plt_words) == 1:
-                plt_words.append(word)
-                continue
-            elif pos == '名詞' and len(plt_words) == 2:
-                plt_words.append(word)
-                print(''.join(plt_words))
-            plt_words = []
+            # pos = pos.split('-')[0]
+            if pos == '名詞-サ変接続':
+                print(word)
